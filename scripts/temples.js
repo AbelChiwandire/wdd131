1f779ba06b596968136457042c28f0986a67d677
AbelChiwandire <achiwandire1@byupathway.edu>
2025-09-12 11:35:13 +0200

    edited to use css variables

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,9 +2,9 @@ const menuButton = document.getElementById('menu');
 const nav = document.querySelector('nav');
 
 menuButton.addEventListener('click', () => {
-  // toggle the hamburger/X icon
+  
   menuButton.classList.toggle('open');
 
-  // toggle the nav visibility
+
   nav.classList.toggle('open');
 });
