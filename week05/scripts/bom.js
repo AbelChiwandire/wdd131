793cd955e93b9248956e84249d851b49b37048f9
AbelChiwandire <achiwandire1@byupathway.edu>
2025-10-04 14:41:49 +0200

    local storage practice with bom project

---
+++ week05/scripts/bom.js
@@ -0,0 +1,56 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+let chaptersArray= getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter)
+});
+
+button.addEventListener("click", () => {
+    if(input.value !== ""){
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = "";
+        input.focus();
+    }
+})
+
+function displayList(item) 
+{
+    const li = document.createElement("li");
+    const deleteBtn = document.createElement("button");
+
+    li.textContent = item;
+    deleteBtn.textContent = "❌";
+    deleteBtn.classList.add('delete');
+
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+
+    deleteBtn.addEventListener("click", () => {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.focus();
+    });
+}
+
+function setChapterList() {
+  localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+  return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+chapter = chapter.slice(0, chapter.length - 1); // this slices off the last character
+
+chaptersArray = chaptersArray.filter((item) => item !== chapter);
+
+function deleteChapter(chapter) {
+  chapter = chapter.slice(0, chapter.length - 1);
+  chaptersArray = chaptersArray.filter(item => item !== chapter);
+  setChapterList();
+}
